390dc9e453bfec28e1baefec28e1946e7a6c7e87
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-03-05 14:51:24 +0900

    Use SCPStatementType type directly in _pledges_t
    The binary layout is the same and it allows:
    - better safety through the use of final switch
    - safer deserialization, as the type is limited to the enum values
    - better stringification by default

--- source/scpd/types/Stellar_SCP.d
+++ source/scpd/types/Stellar_SCP.d
@@ -104,7 +104,7 @@ struct SCPStatement {
         /// Support (de)serialization from Vibe.d
         extern(D) string toString () const @trusted
         {
-            switch (this.type_)
+            final switch (this.type_)
             {
             case SCPStatementType.SCP_ST_PREPARE:
                 return "0" ~ serializeToJsonString(this.prepare_);
@@ -114,8 +114,6 @@ struct SCPStatement {
                 return "2" ~ serializeToJsonString(this.externalize_);
             case SCPStatementType.SCP_ST_NOMINATE:
                 return "3" ~ serializeToJsonString(this.nominate_);
-            default:
-                assert(0);
             }
         }
 
