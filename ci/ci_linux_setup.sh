304e02d35aa7c62561cb031781e3668b9d4e98ec
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-05-22 17:50:27 +0900

    Disable linux setup

--- ci/ci_linux_setup.sh
+++ ci/ci_linux_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -xeu
-set -o pipefail
+# set -xeu
+# set -o pipefail
 
-mkdir -p $HOME/bin/
-ln -s `which gcc-9` $HOME/bin/gcc # /usr/bin/gcc-9
-ln -s `which g++-9` $HOME/bin/g++ # /usr/bin/g++-9
+# mkdir -p $HOME/bin/
+# ln -s `which gcc-9` $HOME/bin/gcc # /usr/bin/gcc-9
+# ln -s `which g++-9` $HOME/bin/g++ # /usr/bin/g++-9
 
-pushd $HOME
-wget https://github.com/jedisct1/libsodium/archive/1.0.18-RELEASE.tar.gz
-tar xvfz 1.0.18-RELEASE.tar.gz
-cd libsodium-1.0.18-RELEASE
-./configure
-make -j4
-sudo make install
-sudo ldconfig # Refresh cache
-popd
+# pushd $HOME
+# wget https://github.com/jedisct1/libsodium/archive/1.0.18-RELEASE.tar.gz
+# tar xvfz 1.0.18-RELEASE.tar.gz
+# cd libsodium-1.0.18-RELEASE
+# ./configure
+# make -j4
+# sudo make install
+# sudo ldconfig # Refresh cache
+# popd
