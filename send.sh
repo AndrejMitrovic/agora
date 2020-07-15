a60616ab4e851e8a14fbbbc05e20c3b565efe43c
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-07-15 17:29:54 +0900

    Add send script

---
+++ send.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+#  ./ssh.exp password 192.168.1.11 id
+set pass [lrange $argv 0 0]
+set server [lrange $argv 1 1]
+set name [lrange $argv 2 2]
+
+spawn scp build/agora-unittests $name@$server:/Users/andrejmitrovic/dev/
+spawn scp /cores/* $name@$server:/Users/andrejmitrovic/dev/
+match_max 100000
+expect "*?assword:*"
+send -- "$pass\r"
+send -- "\r"
+interact
+
