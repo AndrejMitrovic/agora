e99fffd962aefaf4ca290541ef0e3b83ddb6c39d
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-07-23 11:00:46 +0900

    Use old runner script

--- ci/run.sh
+++ ci/run.sh
@@ -3,7 +3,13 @@
 set -xeu
 set -o pipefail
 
-# echo dub build -c unittest -b unittest-cov --skip-registry=all --compiler=ldc2
-dub build -c unittest -b unittest-cov --skip-registry=all --compiler=ldc2
-rdmd --build-only -of./runner --compiler=${DC} ./ci/unittest_runner.d
-sudo ./runner
+# Only build the unittest binary, but don't run it
+# We want to run it ourselves to catch any bug / set timeout, etc...
+dub build -b unittest-cov -c unittest --skip-registry=all --compiler=${DC}
+
+dchatty=1
+# A run currently (2020-07-21) takes < 6 minutes on Linux
+# Try a total of three times
+timeout -s SEGV 8m ./build/agora-unittests
+
+rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
