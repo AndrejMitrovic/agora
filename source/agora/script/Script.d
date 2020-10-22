050c629f1926cfcffee862ae9fc7c6630c158a1e
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-10-22 19:18:53 +0900

    Move nested function outside
    It's much more readable this way.

--- source/agora/script/Script.d
+++ source/agora/script/Script.d
@@ -81,38 +81,6 @@ public struct Script
 
         // todo: add script size checks (based on consensus params)
 
-        string isInvalidPushReason (OP op)()
-        {
-            static assert(op == OP.PUSH_DATA_1 || op == OP.PUSH_DATA_2);
-            alias T = Select!(op == OP.PUSH_DATA_1, ubyte, ushort);
-            if (bytes.length < T.sizeof)
-            {
-                static immutable err1 = op.to!string ~ " opcode requires "
-                    ~ T.sizeof.to!string ~ " byte(s) for the payload size";
-                return err1;
-            }
-
-            const T size = littleEndianToNative!T(bytes[0 .. T.sizeof]);
-            if (size == 0 || size > MAX_STACK_ITEM_SIZE)
-            {
-                static immutable err2 = op.to!string
-                    ~ " opcode requires payload size value to be between 1 and "
-                    ~ MAX_STACK_ITEM_SIZE.to!string;
-                return err2;
-            }
-
-            bytes.popFrontN(T.sizeof);
-            if (bytes.length < size)
-            {
-                static immutable err3 = op.to!string
-                    ~ " opcode payload size exceeds total script size";
-                return err3;
-            }
-
-            bytes.popFrontN(size);
-            return null;
-        }
-
         while (!bytes.empty())
         {
             OP opcode;
@@ -123,12 +91,12 @@ public struct Script
             switch (opcode)
             {
                 case OP.PUSH_DATA_1:
-                    if (auto reason = isInvalidPushReason!(OP.PUSH_DATA_1))
+                    if (auto reason = isInvalidPushReason!(OP.PUSH_DATA_1)(bytes))
                         return reason;
                     else break;
 
                 case OP.PUSH_DATA_2:
-                    if (auto reason = isInvalidPushReason!(OP.PUSH_DATA_2))
+                    if (auto reason = isInvalidPushReason!(OP.PUSH_DATA_2)(bytes))
                         return reason;
                     else break;
 
@@ -144,6 +112,52 @@ public struct Script
             }
         }
 
+        return null;
+    }
+
+    /***************************************************************************
+
+        Checks the validity of a `PUSH_DATA_*` opcode and advances
+        the `bytes` array if the payload does not exceed the array.
+
+        Params:
+            bytes = the opcode byte array
+
+        Returns:
+            null if the opcode is syntactically valid,
+            otherwise the string explaining the reason why it's invalid
+
+    ***************************************************************************/
+
+    private static string isInvalidPushReason (OP op)(ref const(ubyte)[] bytes)
+    {
+        static assert(op == OP.PUSH_DATA_1 || op == OP.PUSH_DATA_2);
+        alias T = Select!(op == OP.PUSH_DATA_1, ubyte, ushort);
+        if (bytes.length < T.sizeof)
+        {
+            static immutable err1 = op.to!string ~ " opcode requires "
+                ~ T.sizeof.to!string ~ " byte(s) for the payload size";
+            return err1;
+        }
+
+        const T size = littleEndianToNative!T(bytes[0 .. T.sizeof]);
+        if (size == 0 || size > MAX_STACK_ITEM_SIZE)
+        {
+            static immutable err2 = op.to!string
+                ~ " opcode requires payload size value to be between 1 and "
+                ~ MAX_STACK_ITEM_SIZE.to!string;
+            return err2;
+        }
+
+        bytes.popFrontN(T.sizeof);
+        if (bytes.length < size)
+        {
+            static immutable err3 = op.to!string
+                ~ " opcode payload size exceeds total script size";
+            return err3;
+        }
+
+        bytes.popFrontN(size);
         return null;
     }
 }
