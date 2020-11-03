a792e5b9216339d1df03438043c54abd4214ebca
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-11-03 11:41:35 +0900

    Engine: Implement OP.VERIFY_INPUT_LOCK

--- source/agora/script/Opcodes.d
+++ source/agora/script/Opcodes.d
@@ -96,12 +96,15 @@ public enum OP : ubyte
     /// Verify the time lock of a spending Transaction
     VERIFY_TX_LOCK = 0x4E,
 
+    /// Verify the time lock of the associated spending Input
+    VERIFY_INPUT_LOCK = 0x4F,
+
     /// Pops two items from the stack. The two items must be a Point (Schnorr),
     /// and a Signature. If the items cannot be deserialized as a Point and
     /// Signature, the script validation fails.
     /// The signature is then validated using Schnorr, if the signature is
     /// valid then `TRUE` is pushed to the stack.
-    CHECK_SIG = 0x4F,
+    CHECK_SIG = 0x5A,
 }
 
 /*******************************************************************************
